51344d8c5853346b75d52adc51a8fa63bdf5e4ce
heli <heli@sogoodapp.com>
2017-07-15 16:42:59 +0800

    fix Time Limit Exceeded

--- problems/11.Container With Most Water/maxArea.js
+++ problems/11.Container With Most Water/maxArea.js
@@ -1,15 +1,15 @@
 /**
  * @param {number[]} height
  * @return {number}
- * Time Limit Exceeded
  */
 var maxArea = function(height) {
-    let max = 0;
-    for (let len=1; len<height.length; len++) {
-        for (let i=0; i+len<height.length; i++) {
-            let j = i+len, v;
-            v = Math.min(height[i], height[j]) * (j-i);
-            max = v > max ? v : max;
+    let max = 0, l = 0, r = height.length-1;
+    while (l < r) {
+        max = Math.max(Math.min(height[l], height[r])*(r-l), max);
+        if (height[l] < height[r]) {
+            l++;
+        } else {
+            r--;
         }
     }
     return max;
