bb51cee8929a0dc1d83804d5587439a02eba9686
legendhl <heli@sogoodapp.com>
2017-07-16 22:42:20 +0800

    two sum better solution

--- problems/1.Two Sum/twoSum.js
+++ problems/1.Two Sum/twoSum.js
@@ -2,15 +2,25 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * Time O(n2)
+ * Time O(n)
  * Space O(1)
  */
 var twoSum = function(nums, target) {
-    let len = nums.length;
+    let map = {}, len = nums.length;
     for (let i=0; i<len; i++) {
-        for (let j=i+1; j<len; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
+        if (map[nums[i]]) {
+            map[nums[i]].push(i);
+        } else {
+            map[nums[i]] = [i];
+        }
+    }
+    for (let i=0; i<len; i++) {
+        let n = target - nums[i];
+        if (map[n]) {
+            for (let j=0; j<map[n].length; j++) {
+                if (map[n][j] !== i) {
+                    return [i, map[n][j]];
+                }
             }
         }
     }
