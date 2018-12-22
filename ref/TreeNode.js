1225d34c20e5ee48efae160cf5589dfa9f4e5d7f
legendhl <legendhl@gmail.com>
2018-12-22 10:22:08 +0800

    revert array to tree

--- ref/TreeNode.js
+++ ref/TreeNode.js
@@ -2,3 +2,16 @@ function TreeNode(val) {
     this.val = val;
     this.left = this.right = null;
 }
+
+function arrayToTree(nodes) {
+    if (!nodes || !nodes.length) return null;
+    let treeNodes = nodes.map(val => val === null || val === undefined ? null : new TreeNode(val));
+    for (let i = 0; i < Math.floor(treeNodes.length / 2); i++) {
+        let node = treeNodes[i];
+        node.left = treeNodes[i * 2 + 1];
+        if (i * 2 + 2 < treeNodes.length) {
+            node.right = treeNodes[i * 2 + 2];
+        }
+    }
+    return treeNodes;
+}
