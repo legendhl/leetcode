e3df637fbf3813253ea88f1c6736fa075dca801d
legendhl <legendhl@gmail.com>
2019-03-08 22:43:18 +0800

    860. Lemonade Change

---
+++ problems/860. Lemonade Change/lemonadeChange.js
@@ -0,0 +1,72 @@
+/**
+At a lemonade stand, each lemonade costs $5. 
+
+Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+
+Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+
+Note that you don't have any change in hand at first.
+
+Return true if and only if you can provide every customer with correct change.
+
+Example 1:
+Input: [5,5,5,10,20]
+Output: true
+Explanation: 
+From the first 3 customers, we collect three $5 bills in order.
+From the fourth customer, we collect a $10 bill and give back a $5.
+From the fifth customer, we give a $10 bill and a $5 bill.
+Since all customers got correct change, we output true.
+
+Example 2:
+Input: [5,5,10]
+Output: true
+
+Example 3:
+Input: [10,10]
+Output: false
+
+Example 4:
+Input: [5,5,10,10,20]
+Output: false
+Explanation: 
+From the first two customers in order, we collect two $5 bills.
+For the next two customers in order, we collect a $10 bill and give back a $5 bill.
+For the last customer, we can't give change of $15 back because we only have two $10 bills.
+Since not every customer received correct change, the answer is false.
+ 
+Note:
+
+0 <= bills.length <= 10000
+bills[i] will be either 5, 10, or 20.
+ */
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ * Runtime: 80 ms, faster than 39.57%
+ * Memory Usage: 36 MB, less than 73.33%
+ */
+var lemonadeChange = function (bills) {
+    let changesCount = [0, 0]; //5, 10
+    for (let i = 0; i < bills.length; i++) {
+        let bill = bills[i];
+        if (bill === 5) {
+            changesCount[0]++;
+        } else if (bill === 10) {
+            changesCount[1]++;
+        }
+        let change = bill - 5;
+        while (change >= 10 && changesCount[1]) {
+            change -= 10;
+            changesCount[1]--;
+        }
+        while (change >= 5 && changesCount[0]) {
+            change -= 5;
+            changesCount[0]--;
+        }
+        if (change !== 0) {
+            return false;
+        }
+    }
+    return true;
+};
