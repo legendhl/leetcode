0beeaf4f3213435a7756cab46af8ce949282202c
legendhl <legendhl@gmail.com>
2019-04-16 22:53:27 +0800

    QuadTreeNode

---
+++ ref/QuadTreeNode.js
@@ -0,0 +1,8 @@
+function Node(val, isLeaf, topLeft, topRight, bottomLeft, bottomRight) {
+    this.val = val;
+    this.isLeaf = isLeaf;
+    this.topLeft = topLeft;
+    this.topRight = topRight;
+    this.bottomLeft = bottomLeft;
+    this.bottomRight = bottomRight;
+};
