2e74e7feeef104344302bf9256c6a4d57a88ac7b
legendhl <legendhl@gmail.com>
2019-04-22 21:31:50 +0800

    1030. Matrix Cells in Distance Order

---
+++ problems/1030. Matrix Cells in Distance Order/allCellsDistOrder.js
@@ -0,0 +1,60 @@
+/**
+We are given a matrix with R rows and C columns has cells with integer coordinates (r, c), where 0 <= r < R and 0 <= c < C.
+
+Additionally, we are given a cell in that matrix with coordinates (r0, c0).
+
+Return the coordinates of all cells in the matrix, sorted by their distance from (r0, c0) from smallest distance to largest distance.  Here, the distance between two cells (r1, c1) and (r2, c2) is the Manhattan distance, |r1 - r2| + |c1 - c2|.  (You may return the answer in any order that satisfies this condition.)
+
+Example 1:
+Input: R = 1, C = 2, r0 = 0, c0 = 0
+Output: [[0,0],[0,1]]
+Explanation: The distances from (r0, c0) to other cells are: [0,1]
+
+Example 2:
+Input: R = 2, C = 2, r0 = 0, c0 = 1
+Output: [[0,1],[0,0],[1,1],[1,0]]
+Explanation: The distances from (r0, c0) to other cells are: [0,1,1,2]
+The answer [[0,1],[1,1],[0,0],[1,0]] would also be accepted as correct.
+
+Example 3:
+Input: R = 2, C = 3, r0 = 1, c0 = 2
+Output: [[1,2],[0,2],[1,1],[0,1],[1,0],[0,0]]
+Explanation: The distances from (r0, c0) to other cells are: [0,1,1,2,2,3]
+There are other answers that would also be accepted as correct, such as [[1,2],[1,1],[0,2],[1,0],[0,1],[0,0]].
+
+Note:
+
+1 <= R <= 100
+1 <= C <= 100
+0 <= r0 < R
+0 <= c0 < C
+ */
+/**
+ * @param {number} R
+ * @param {number} C
+ * @param {number} r0
+ * @param {number} c0
+ * @return {number[][]}
+ * Runtime: 184 ms, faster than 53.49%
+ * Memory Usage: 53.2 MB, less than 100.00%
+ */
+var allCellsDistOrder = function (R, C, r0, c0) {
+    let distances = new Array(R + C - 1);
+    let res = [];
+    for (let i = 0; i < R; i++) {
+        for (let j = 0; j < C; j++) {
+            let index = Math.abs(i - r0) + Math.abs(j - c0);
+            if (distances[index]) {
+                distances[index].push([i, j]);
+            } else {
+                distances[index] = [[i, j]];
+            }
+        }
+    }
+    for (let i = 0; i < distances.length; i++) {
+        if (distances[i]) {
+            res = res.concat(distances[i]);
+        }
+    }
+    return res;
+};
